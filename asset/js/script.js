d5c83f7ed8e6044373e83ea29d26991d46cf2b27
FabioVasi <vasifabio96@gmail.com>
2023-09-20 14:23:31 +0200

    add cycle and list tag

--- asset/js/script.js
+++ asset/js/script.js
@@ -18,10 +18,13 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      randomEmails: ''
+      randomEmails: []
     }
   },
   mounted() {
+    for (let i = 0; i < 10; i++) {
+        
+    }
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => {
         console.log(response);
