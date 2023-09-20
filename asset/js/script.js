ee2e958deee3d96757172d47bb4eb90dac8b5564
FabioVasi <vasifabio96@gmail.com>
2023-09-20 14:34:42 +0200

    add complete cycle and v-for

--- asset/js/script.js
+++ asset/js/script.js
@@ -23,12 +23,13 @@ createApp({
   },
   mounted() {
     for (let i = 0; i < 10; i++) {
-        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response);
+            this.randomEmails = response.data.response;
+            
+        })
     }
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => {
-        console.log(response);
-        this.randomEmails = response.data.response;
-    })
+    
   }
 }).mount('#app')
